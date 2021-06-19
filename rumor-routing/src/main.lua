90b6f3c94fb0917782468911de7115b910a4908a
matheus.leal <matheus.leal>
2021-06-19 19:33:29 -0300

    commit update

--- rumor-routing/src/main.lua
+++ rumor-routing/src/main.lua
@@ -39,22 +39,22 @@ end
 function mqttcb(topic, message)
   local data_received = json.decode(message)
   if not (n.id == data_received['id']) then
-    -- Mensagem hello novo nó
+    -- message hello for new node
     if data_received['method'] == "hello" and node.new_neighbor(n.neighbors, data_received['id']) then
-      -- Adiciona vizinho ao nó
+      -- add neighbor to node
       node.add_neighbor(n.neighbors, data_received['id'])
-      -- Envia mensagem hello de volta para o vizinho
+      -- send hello message back to neighbor
       data = mqtt_request_message(n, n.id, data_received['method'])
       mqtt_client:publish(topic, json.encode(data))
-      -- Envia mensagem para o console
+      -- send message to console
       message_log =  "Node " .. data_received['id'] .. " became a neighbor"
       update_console_log(n.id, message_log)
-    -- Mensagem para evento 1
+    -- message for event 1
     elseif data_received['method'] == "evento_1" and node.new_event(n.events, data_received['method']) then
-      -- Adiciona novo evento
+      -- add new event
       current_distance = node.get_current_distance(data_received['node'].events, data_received['method'])
       node.event_detected(n, data_received['method'], topic, current_distance + 1)
-      -- Envia mensagem para os outros canais
+      -- send message to other channels
       for c in pairs(n.channels) do
         channel = n.channels[c]
         if not (channel == topic) then
@@ -62,15 +62,15 @@ function mqttcb(topic, message)
           mqtt_client:publish(channel, json.encode(data))
         end
       end
-      -- Envia mensagem para o console
+      -- send message to console
       message_log =  "Node " .. data_received['id'] .. " sent " .. data_received['method']
       update_console_log(n.id, message_log)
-    -- Mensagem para evento 2
+    -- message for event 2
     elseif data_received['method'] == "evento_2" and node.new_event(n.events, data_received['method']) then
-      -- Adiciona novo evento
+      -- add new event
       current_distance = node.get_current_distance(data_received['node'].events, data_received['method'])
       node.event_detected(n, data_received['method'], topic, current_distance + 1)
-      -- Envia mensagem para os outros canais
+      -- send message to other channels
       for c in pairs(n.channels) do
         channel = n.channels[c]
         if not (channel == topic) then
@@ -78,10 +78,10 @@ function mqttcb(topic, message)
           mqtt_client:publish(channel, json.encode(data))
         end
       end
-      -- Envia mensagem para o console
+      -- send message to console
       message_log =  "Node " .. data_received['id'] .. " sent " .. data_received['method']
       update_console_log(n.id, message_log)
-    -- Mensagem para consulta do evento 1
+    -- event 1 query message
     elseif data_received['method'] == "consulta_1" and agent.new_channel(data_received['node'].channels, topic) then
       if node.get_current_distance(n.events, data_received['node'].event) > 0 then
         for c in pairs(n.channels) do
@@ -97,10 +97,10 @@ function mqttcb(topic, message)
         print("cheguei")
         agent.add_channel(data_received['node'].channels, topic)
         agent.add_track(data_received['node'].track, n.id)
-        -- Envia mensagem para o console
+        -- send message to console
         message_log =  "Returning request for event 1"
         update_console_log(n.id, message_log)
-        -- Retornando request da consulta
+        -- returning query request
         data_received['node'].channels = {}
         data = mqtt_request_message(data_received['node'], n.id, "resposta_consulta_1")
         for c in pairs(n.channels) do
@@ -108,7 +108,7 @@ function mqttcb(topic, message)
           mqtt_client:publish(channel, json.encode(data))
         end
       end
-    -- Mensagem para consulta do evento 2
+    -- event 2 query message
     elseif data_received['method'] == "consulta_2" and agent.new_channel(data_received['node'].channels, topic) then
       if node.get_current_distance(n.events, data_received['node'].event) > 0 then
         for c in pairs(n.channels) do
@@ -124,10 +124,10 @@ function mqttcb(topic, message)
         print("cheguei")
         agent.add_channel(data_received['node'].channels, topic)
         agent.add_track(data_received['node'].track, n.id)
-        -- Envia mensagem para o console
+        -- send message to console
         message_log =  "Returning request for event 2"
         update_console_log(n.id, message_log)
-        -- Retornando request da consulta
+        -- returning query request
         data_received['node'].channels = {}
         data = mqtt_request_message(data_received['node'], n.id, "resposta_consulta_2")
         for c in pairs(n.channels) do
@@ -135,10 +135,10 @@ function mqttcb(topic, message)
           mqtt_client:publish(channel, json.encode(data))
         end
       end
-    -- Mensagem para retorno da consulta do evento 1
+    -- event 1 query return message
     elseif data_received['method'] == "resposta_consulta_1" and agent.new_channel(data_received['node'].channels, topic) then
       if n.id == data_received['node'].id then
-        -- Envia mensagem para o console
+        -- send message to console
         message_log =  "Got the following track for event 1 " .. json.encode(data_received['node'].track)
         update_console_log(n.id, message_log)
       else
@@ -151,10 +151,10 @@ function mqttcb(topic, message)
           end
         end
       end
-    -- Mensagem para retorno da consulta do evento 2
+    -- event 2 query return message
     elseif data_received['method'] == "resposta_consulta_2" and agent.new_channel(data_received['node'].channels, topic) then
       if n.id == data_received['node'].id then
-        -- Envia mensagem para o console
+        -- send message to console
         message_log =  "Got the following track for event 2 " .. json.encode(data_received['node'].track)
         update_console_log(n.id, message_log)
       else
@@ -171,7 +171,7 @@ function mqttcb(topic, message)
   end
 end
 
--- Reuse code for multiples servers
+-- reuse code for multiples servers
 if #arg < 1 then
   print("Error: missing argument(s).\nUsage: love src [channels]")
   os.exit()
@@ -185,7 +185,7 @@ channels = str_split(arg[2], ",")
 function love.mousepressed(x, y)
   if button.click(x, y, bt1) then
     node.event_detected(n, "evento_1", n.channels, 0)
-    -- Envia mensagem de evento para vizinhos
+    -- send event message to neighbors
     data = mqtt_request_message(n, n.id, "evento_1")
     for c in pairs(n.channels) do
       channel = n.channels[c]
@@ -193,67 +193,67 @@ function love.mousepressed(x, y)
     end
   elseif button.click(x, y, bt2) then
     node.event_detected(n, "evento_2", n.channels, 0)
-    -- Envia mensagem de evento para vizinhos
+    -- send event message to neighbors
     data = mqtt_request_message(n, n.id, "evento_2")
     for c in pairs(n.channels) do
       channel = n.channels[c]
       mqtt_client:publish(channel, json.encode(data))
     end
   elseif button.click(x, y, bt3) then
-    -- Cria agente para busca do evento 1
+    -- create agent to search for event 1
     a = agent.create(n.id, "evento_1", {})
-    -- Envia mensagem para todos vizinhos
+    -- send message to all neighbors
     data = mqtt_request_message(a, n.id, "consulta_1")
     for c in pairs(n.channels) do
       channel = n.channels[c]
       agent.add_track(a.track, n.id)
       mqtt_client:publish(channel, json.encode(data))
     end
-    -- Envia mensagem para o console
+    -- send message to console
     message_log =  "Checking for the event 1"
     update_console_log(n.id, message_log)
   elseif button.click(x, y, bt4) then
-    -- Cria agente para busca do evento 2
+    -- create agent to search for event 2
     a = agent.create(n.id, "evento_2", {})
-    -- Envia mensagem para todos vizinhos
+    -- send message to all neighbors
     data = mqtt_request_message(a, n.id, "consulta_2")
     for c in pairs(n.channels) do
       channel = n.channels[c]
       agent.add_track(a.track, n.id)
       mqtt_client:publish(channel, json.encode(data))
     end
-    -- Envia mensagem para o console
+    -- send message to console
     message_log =  "Checking for the event 2"
     update_console_log(n.id, message_log)
   elseif button.click(x, y, bt5) then
-    -- Envia mensagem para o console
+    -- send message to console
     message_log = json.encode(n)
     update_console_log(n.id, message_log)
   end
 end
 
 function love.load()
-  -- Inicializa botões
+  -- initialize buttons
   bt1 = button.create("evento 1", 5, 35, 75, 60)
   bt2 = button.create("evento 2", 83.5, 35, 75, 60)
   bt3 = button.create("consulta 1", 162.5, 35, 75, 60)
   bt4 = button.create("consulta 2", 241, 35, 75, 60)
   bt5 = button.create("estado", 320, 35, 75, 60)
-  -- Inicia nó
+  -- initialize node
   n = node.create(channels)
-  -- Setup conexão mqtt
+  -- setup mqtt connection
   client_id = "cliente_mqtt_" .. n.id
   mqtt_client = mqtt.client.create("localhost", 1883, mqttcb)
-  -- Inicializa logs
+  -- initialize logs
   text = ""
   logs_size = 0
   log.initialize_log(n.id)
-  -- Inicia conexão mqtt
+  -- initialize mqtt connection
   for c in pairs(n.channels) do
     channel = n.channels[c]
     mqtt_client:connect(client_id)
     mqtt_client:subscribe({channel})
-    -- Envia mensagem de hello para os vizinhos
+    -- send hello message to neighbors
     data = mqtt_request_message(n, n.id, "hello")
     mqtt_client:publish(channel, json.encode(data))
     print(client_id .. " started on " .. channel)
@@ -261,20 +261,20 @@ function love.load()
 end
 
 function love.draw()
-  -- Desenha Fundo
+  -- draw Background
   love.graphics.setColor(0.92,0.93,0.93)
   love.graphics.rectangle("fill", 4, 4, 392, 292)
-  -- Desenha Botões
+  -- draw buttons
   button.draw(0.18, 0.8, 0.443, bt1.x, bt1.y, bt1.width, bt1.height, bt1.text)
   button.draw(0.18, 0.8, 0.443, bt2.x, bt2.y, bt2.width, bt2.height, bt2.text)
   button.draw(0.2, 0.596, 0.86, bt3.x, bt3.y, bt3.width, bt3.height, bt3.text)
   button.draw(0.2, 0.596, 0.86, bt4.x, bt4.y, bt4.width, bt4.height, bt4.text)
   button.draw(0.96, 0.69, 0.255, bt5.x, bt5.y, bt5.width, bt5.height, bt5.text)
-  -- Desenha Caixa de Log 
+  -- draw console
   love.graphics.print("log", 20, 110)
   love.graphics.setColor(0.337,0.396,0.45)
   love.graphics.rectangle("fill", 20, 130, 360, 150)
-  -- Desenha Logs
+  -- draw logs
   love.graphics.setColor(0.92,0.93,0.93)
   love.graphics.print(text, 20, 130)
 end
