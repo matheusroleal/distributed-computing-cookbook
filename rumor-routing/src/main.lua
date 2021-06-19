6e6807fddea91ae6afac4cc80ed47e58d18b0c40
matheus.leal <matheus.leal>
2021-06-18 22:55:49 -0300

    rummor routing neighbour conection

--- rumor-routing/src/main.lua
+++ rumor-routing/src/main.lua
@@ -1,53 +1,85 @@
+local json = require "json"
 local mqtt = require("mqtt_library")
 local button = require("button")
+local node = require("node")
 
 math.randomseed(os.time())
 
-function mqttcb(topic, message)
+function exibe_no_log(message)
   if (logs_size + 1) > 10 then
     logs_size = 0
     text = ""
   end
   text = text .. message
   logs_size = logs_size + 1
-  if message == "a" then 
-    controle = not controle
+end
+
+function mqttcb(topic, message)
+  local data_received = json.decode(message)
+  if not (n.id == data_received['id']) then
+    if data_received['method'] == "hello" then
+      node.addNeighbor(n.neighbors, data_received['id'])
+      message_log =  "Node " .. data_received['id'] .. " became a neighbor\n"
+    elseif data_received['method'] == "evento_1" then
+      node.eventDetected(n, data_received['method'], topic)
+      message_log =  "Node " .. data_received['id'] .. " sent " .. data_received['method'] .. "\n"
+    elseif data_received['method'] == "evento_2" then
+      node.eventDetected(n, data_received['method'], topic)
+      message_log =  "Node " .. data_received['id'] .. " sent " .. data_received['method'] .. "\n"
+    end
+    -- Imprime mensagem no log
+    exibe_no_log(message_log)
   end
 end
 
 function love.mousepressed(x, y)
-  if (logs_size + 1) > 10 then
-    logs_size = 0
-    text = ""
-  end
+  data = {}
+  data['node'] = n
+  data['id'] = n.id
   if button.click(x, y, bt1) then
-    mqtt_client:publish("controle", "pressionou botao evento 1 \n")
-  end
-  if button.click(x, y, bt2) then
-    mqtt_client:publish("controle", "pressionou botao evento 2 \n")
-  end
-  if button.click(x, y, bt3) then
-    mqtt_client:publish("controle", "pressionou botao consulta \n")
-  end
-  if button.click(x, y, bt4) then
-    mqtt_client:publish("controle", "pressionou botao evento \n")
+    data['method'] = "evento_1"
+    mqtt_client:publish("controle", json.encode(data))
+  elseif button.click(x, y, bt2) then
+    data['method'] = "evento_2"
+    mqtt_client:publish("controle", json.encode(data))
+  elseif button.click(x, y, bt3) then
+    message_log =  "Checking for the event\n"
+    exibe_no_log(message_log)
+  elseif button.click(x, y, bt4) then
+    message_log = json.encode(data)
+    exibe_no_log(message_log)
   end
 end
 
 function love.load()
+  channels = {"controle"}
   -- Inicializa botões
   bt1 = button.create("evento 1", 20, 35, 75, 60)
   bt2 = button.create("evento 2", 115, 35, 75, 60)
   bt3 = button.create("consulta", 210, 35, 75, 60)
-  bt4 = button.create("evento", 305, 35, 75, 60)
+  bt4 = button.create("estado", 305, 35, 75, 60)
   -- Inicializa logs
   text = ""
   logs_size = 0
-  -- Inicia conexão mqtt
-  client_id = "cliente_mqtt_" .. math.random()
-  mqtt_client = mqtt.client.create("localhost", 1883, mqttcb) 
-  mqtt_client:connect(client_id)
-  mqtt_client:subscribe({"controle"})
+  -- Inicia nó
+  n = node.create(channels)
+  -- Setup conexão mqtt
+  client_id = "cliente_mqtt_" .. n.id
+  mqtt_client = mqtt.client.create("localhost", 1883, mqttcb)
+  for c in pairs(n.channels) do
+    channel = n.channels[c]
+    print(channel)
+    mqtt_client:connect(client_id)
+    -- Inicia conexão mqtt
+    mqtt_client:subscribe({channel})
+    -- Envia mensagem de hello para os vizinhos
+    data = {}
+    data['method'] = "hello"
+    data['node'] = n
+    data['id'] = n.id
+    mqtt_client:publish(channel, json.encode(data))
+    print(client_id .. " started on " .. channel)
+  end
 end
 
 function love.draw()
---
+++ rumor-routing/src/node.lua
@@ -0,0 +1,60 @@
+local node = {}
+
+math.randomseed(os.time())
+
+function node.create(neighbors_channel)
+    my_node = {}
+    my_node.id = math.random(0,1000)
+    my_node.channels = neighbors_channel
+    my_node.neighbors = {}
+    my_node.events = {}
+    return my_node
+end
+
+function node.addNeighbor(neighbors, neighbor)
+    table.insert(neighbors, neighbor)
+end
+
+function node.eventDetected(my_node, event_id, channel)
+    event = {}
+    event.distance = 0
+    event.id = event_name
+    event.channel = channel
+    table.insert(my_node.events, event)
+    -- ForwardAgent(node.ID)
+end
+
+function node.agentReceived(my_node, agent, source)
+    agent.num_hops = agent.num_hops + 1
+    -- update the node's events table based on the agent's
+    for e in pairs(agent.events) do
+        if not (my_node.events[e]) or (my_node.events[e].num_hops > agent.events[e]) then
+            my_node.events[e].distance = agent.num_hops - agent.events[e].visit_time
+            agent.events[e].direction = source
+        end
+    end
+    -- update the agent's events table based on the node's
+    for e in pairs(my_node.events) do
+        agent.events[e].visit_time = (- my_node.events[e].distance)
+    end
+    if agent.num_hops < agent_ttl then
+        -- destination = pick neighbor based on agent forwarding policy
+        forwardAgent(agent, destination)
+    end
+end
+
+function node.queryReceived(my_node, query, source)
+    query.ttl = query.ttl - 1
+    -- the query reached a valid destination
+    if my_node.events[query.event_name].distance == 0 then
+        handleValidQuery(query)
+    -- the node has a path to the event
+    elseif my_node.events[query.event_name].distance > 0 then
+        forwardQuery(query, my_node.events[query.event_name].direction)
+    else
+        -- destination = pick neighbor based on query forwarding policy
+        forwardQuery(query, destination)
+    end
+end
+
+return node
